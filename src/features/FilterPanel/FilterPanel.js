025a4bb48bb10d5fa6effb939b587e0a1c818002
Andrey Kazakov <andreyalexandrovich.k@gmail.com>
2022-04-18 09:53:46 +0300

    filterPanel fixes

--- src/features/FilterPanel/FilterPanel.js
+++ src/features/FilterPanel/FilterPanel.js
@@ -1,35 +1,49 @@
+import { Component } from 'react';
+
 import Button from '../../components/Button/Button';
 import './FilterPanel.scss';
 
 const filtersArr = ['All', 'Active', 'Completed'];
 
-const FilterPanel = ({todosArr, activeFilter, onFilters, clearCompleted}) => {
+class FilterPanel extends Component {
 
-	if(!todosArr.length) {
-		return null;
-	};
+	constructor(props) {
+		super(props);
+		this.id = 1;
+	}
 
-	return(
-		<div className={"todo-filters visible"}>
-		<div className="todo-filters-total">Total: {todosArr.length}</div>
-		<nav className="todo-filters-list">
-			{
-				filtersArr.map((filter) => (
-					<Button
-						onClick={() => onFilters(filter)}
-						className={`todo-filters-item ${filter === activeFilter ? "active-btn" : ""}`}>
-							{filter}
-					</Button>
-				))
-			}
-		</nav>
-		<Button
-			className="todo-filters-clear"
-			onClick={() => clearCompleted()}>
-				Clear completed
-		</Button>
-	</div>
-	)
+
+	render() {
+
+		const { todosArr, activeFilter, onFilters, clearCompleted } = this.props;
+
+		if(!todosArr.length) {
+			return null;
+		};
+
+		return(
+			<div className={"todo-filters visible"}>
+			<div className="todo-filters-total">Total: {todosArr.length}</div>
+			<nav className="todo-filters-list">
+				{
+					filtersArr.map((filter) => (
+						<Button
+							key={this.id++}
+							onClick={() => onFilters(filter)}
+							className={`todo-filters-item ${filter === activeFilter ? "active-btn" : ""}`}>
+								{filter}
+						</Button>
+					))
+				}
+			</nav>
+			<Button
+				className="todo-filters-clear"
+				onClick={() => clearCompleted()}>
+					Clear completed
+			</Button>
+		</div>
+		)
+	}
 }
 
 export default FilterPanel;
